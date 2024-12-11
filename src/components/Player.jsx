3f430a577538a3db328172fb394499f0f68f1be9
Muhammad Bilal <62239033+bilalpirzada@users.noreply.github.com>
2024-12-11 11:13:14 +0500

    Make edit button interactable so player can edit names

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react"
+
 export default function Player({name, symbol}){
+  const [playerName,setPlayerName] = useState(name);
+
+ const [isEditing,setIsEditing] = useState(false);
+
+  function handleClick()
+  {
+    setIsEditing(editing=>!editing);
+  }
+
+  function handleChange(event){
+    setPlayerName(event.target.value);
+  }
+
   return (
     <li>
     <span className="player">
-      <span className="player-name">{name}</span>
+      {isEditing?<input type="text" required value={playerName} onChange={handleChange}/> : <span className="player-name">{playerName}</span>}
       <span className="player-symbol">{symbol}</span>
     </span>
-    <button>Edit</button>
+    <button onClick={handleClick}>{isEditing ? 'Save':'Edit'}</button>
   </li>
   )
 }
